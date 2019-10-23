b8882094ce09a5c6b5f1e8c52e18910ae1f40634
RamySaied1 <ramy.m.saied@gmail.com>
2019-10-23 14:46:32 +0200

    A star + game class

---
+++ search.py
@@ -0,0 +1,75 @@
+from typing import TypeVar, List, Tuple, MutableSet, MutableMapping, Optional
+from problem import Problem
+from queue import PriorityQueue
+from heapq import _siftup, _siftdown, heappop,heappush,heapify
+
+
+State = TypeVar("State")
+Action = TypeVar("Action")
+
+# A Breadth First Search Implementation which takes a problem and optionally an initial state
+# and returns a list of actions to reach the nearest goal or None if there is no solution
+def bfs(problem: Problem[State, Action], initial_state: Optional[State] = None) -> Optional[List[Action]]:
+    initial_state = initial_state or problem.initial_state
+    frontier: List[State] = [initial_state]
+    visited: MutableSet[State] = set()
+    predecessor: MutableMapping[State, Tuple[State, Action]] = {initial_state: (None, None)}
+    while len(frontier) > 0:
+        current_state = frontier.pop(0)
+        if problem.is_goal(current_state):
+            solution = []
+            while True:
+                current_state, action = predecessor[current_state]
+                if action is None:
+                    return solution
+                else:
+                    solution.insert(0, action)
+        if current_state in visited:
+            continue
+        visited.add(current_state)
+        for action in problem.get_actions(current_state):
+            successor, _ = problem.get_successor(current_state, action)
+            #print(action, predecessor)
+            if successor not in predecessor:
+                predecessor[successor] = (current_state, action)
+                frontier.append(successor)
+    return None
+
+
+
+def aStar(problem: Problem[State, Action], initial_state: Optional[State] = None) -> Optional[List[Action]]:
+    initial_state = initial_state or problem.initial_state
+    frontier: List[State] = [initial_state]
+    visited: MutableSet[State] = set()
+    predecessor: MutableMapping[State, Tuple[State, Action]] = {initial_state: (None, None)}
+    while len(frontier) > 0:
+        current_state = heappop(frontier)
+        if problem.is_goal(current_state):
+            solution = []
+            while True:
+                current_state, action = predecessor[current_state]
+                if action is None:
+                    return solution
+                else:
+                    solution.insert(0, action)
+        if current_state in visited:
+            continue
+        visited.add(current_state)
+        for action in problem.get_actions(current_state):
+            successor, cost= problem.get_successor(current_state, action)
+            #print(action, predecessor)
+            if successor not in predecessor:
+                predecessor[successor] = (current_state, action)
+                successor.setPathCost(cost,current_state.getPathCost())
+                successor.setHeuristic()
+                heappush(frontier,successor)
+            elif successor not in visited:
+                i = frontier.index(successor)
+                oldsucc=frontier[i]
+                if (oldsucc.getPathCost()> cost+current_state.getPathCost()):
+                    oldsucc.setPathCost(cost,current_state.getPathCost())
+                    predecessor[oldsucc] = (current_state, action)
+                    oldsucc.setHeuristic()
+                    heapify(frontier)
+
+    return None
---
+++ squirrel.py
@@ -0,0 +1,142 @@
+from typing import TypeVar, Generic, List, Tuple, Optional, MutableMapping, Type, Any
+from problem import Problem,State,Action
+from vector import Vector
+from map2d import Map2D
+
+Directions = {
+    "UP": Vector(0, -1),
+    "DOWN": Vector(0, 1),
+    "LEFT": Vector(-1, 0),
+    "RIGHT": Vector(1, 0),
+    "TAKE":Vector(0,0),
+    "HIDE":Vector(0,0)
+}
+
+GROUND_SYMBOL = "."
+WALL_SYMBOL = "#"
+STASH_SYMBOL = "X"
+AGENT_SYMBOL = "@"
+NUT_SYMBOL = "N"
+
+
+class Squirrel(Problem['Squirrel.State', str]):
+    # Define a state of the Maze problem. This class is immutable and hashable so that it can be used by the search function
+    class State:
+        __slots__ = ("problem", "NutsPositions", "nutsWith",
+                     "pos", "stashPositions", "pathCost", "heuristic")
+        problem: 'Squirrel'
+        NutsPositions: List
+        nutsWith: int
+        pos : Vector
+        stashPositions : List
+        pathCost: int 
+        heuristic: int 
+
+
+
+        def __init__(self, problem: 'Squirrel', NutsPositions: List, nutsWith: int, pos: Vector, stashPositions: List,pathCost :int =0,heuristic:int =-1):
+            super().__setattr__("problem", problem)
+            super().__setattr__("NutsPositions", NutsPositions)
+            super().__setattr__("nutsWith", nutsWith)
+            super().__setattr__("pos", pos)
+            super().__setattr__("stashPositions", stashPositions)
+            super().__setattr__("pathCost", pathCost)
+            super().__setattr__("heuristic", heuristic)
+
+
+
+        def __setattr__(self, name, value):
+            raise NotImplementedError
+
+        def __hash__(self):
+
+            return hash(      (self.pos,tuple(self. NutsPositions),tuple(self.stashPositions))     )
+
+        def __lt__(self, value: 'Squirrel.State') -> bool:
+            return (self.pathCost+self.heuristic < value.pathCost+value.heuristic)
+
+        def __eq__(self, value: 'Squirrel.State') -> bool:
+            return (self.pos) == value.pos and (self.NutsPositions == value.NutsPositions) and (self.stashPositions == value.stashPositions)
+
+        def __str__(self) -> str:
+            return '\n'.join(''.join(AGENT_SYMBOL if self.pos == (x, y) else cell for x, cell in enumerate(row)) for y, row in enumerate(self.problem.grid))
+
+        def setHeuristic(self):
+            if (self.heuristic ==-1):
+                super().__setattr__("heuristic", self.problem.heuristic(self))
+
+
+
+        def setPathCost(self,parentCost,actionCost):
+            super().__setattr__("pathCost", parentCost+actionCost)
+        def getPathCost(self):
+            return self.pathCost
+
+    
+    @classmethod
+    def read_from_file(cls, file_path: str) -> 'Maze':
+        with open(file_path, 'r') as f:
+            grid = [[cell for cell in line.strip()] for line in f.readlines()]
+            
+            height = len(grid)
+            assert height > 0, "Map height must be greater than zero"
+            width = max(len(row) for row in grid)
+            assert width > 0, "Map width must be greater than zero"
+
+            agentPosition = [Vector(x,y) for y, row in enumerate(grid) for x, cell in enumerate(row) if cell == AGENT_SYMBOL]
+            stashPositions = [Vector(x, y) for y, row in enumerate(
+                grid) for x, cell in enumerate(row) if cell == STASH_SYMBOL]
+            NutsPositions = [Vector(x, y) for y, row in enumerate(
+                grid) for x, cell in enumerate(row) if cell == NUT_SYMBOL]
+
+            assert len(agentPosition) == 1, "There must be one agent in the maze"
+            agentPosition = agentPosition[0]
+            
+            grid[agentPosition[1]][agentPosition[0]] = GROUND_SYMBOL
+            for row in grid:
+                row.extend([WALL_SYMBOL]*(width - len(row)))
+            return Squirrel(Map2D(grid), agentPosition, NutsPositions, 0, stashPositions)
+
+    def __init__(self, grid: Map2D,  agentPosition, NutsPositions, nutsWith, stashPositions):
+        super().__init__()
+        self.grid = grid
+        self.initial_pos = (NutsPositions, nutsWith,
+                            agentPosition, stashPositions)
+    # Returns the initial state
+    @property
+    def initial_state(self) -> State:
+        return Squirrel.State(self, *self.initial_pos)
+
+    # Returns a list of possible actions from the given state
+    def get_actions(self, state: State) -> List[Action]:
+        actions=[name for name, direction in Directions.items() if self.grid.inside(state.pos+direction) and self.grid[state.pos+direction] != WALL_SYMBOL]
+        if ((state.pos  not in state.stashPositions) or state.nutsWith<=0):
+            actions.remove("HIDE")
+        if (state.pos  not in state.NutsPositions):
+            actions.remove("TAKE")
+
+        return actions
+
+    # Given a state and a valid action, return the next state and the action cost
+    def get_successor(self, state: State, action: Action) -> Tuple[State, float]:
+        if (action in ["UP","RIGHT","LEFT","DOWN"]):
+            return Squirrel.State(self, state.NutsPositions, state.nutsWith, state.pos+Directions[action], state.stashPositions), 1+((state.nutsWith*(state.nutsWith+1))/2)
+        elif(action =="TAKE"):
+            NutsPositionsNew=list(state.NutsPositions)
+            NutsPositionsNew.remove(state.pos)
+            return Squirrel.State(self, NutsPositionsNew, state.nutsWith+1, state.pos+Directions[action],state.stashPositions) ,1
+        else:
+            stashPositionsNew = list(state.stashPositions)
+            stashPositionsNew.remove(state.pos)
+            
+            return Squirrel.State(self, state.NutsPositions, state.nutsWith-1, state.pos+Directions[action], stashPositionsNew) ,1
+
+
+    # Checks if the given state is a goal
+    def is_goal(self, state: State) -> bool:
+        return (state.nutsWith==0) and (len(state.NutsPositions)==0)
+    
+    # Calculate the heuristic of the given state
+    def heuristic(self, state: State) -> float:
+        return state.nutsWith
+    
